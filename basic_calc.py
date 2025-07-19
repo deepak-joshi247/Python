b2d6143d46e15c984940f5ac50d7bbd4053b3820
deepak-joshi247 <deepaksjoshi247@gmail.com>
2025-07-19 18:47:28 +0530

    printing

--- basic_calc.py
+++ basic_calc.py
@@ -1,20 +1,23 @@
-'''super lazy to do this '''
-while True:
-    num1 = int(input("Enter first the number\n"))
-    num2 = int(input("Enter the second number\n"))
-    opp = int(input("Enter the operation\n 1.add  2. sub  3.mul  4.divide  5.power  6.exit\n"))
+# '''super lazy to do this '''
+# while True:
+#     num1 = int(input("Enter first the number\n"))
+#     num2 = int(input("Enter the second number\n"))
+#     opp = int(input("Enter the operation\n 1.add  2. sub  3.mul  4.divide  5.power  6.exit\n"))
     
-    if(opp==1):
-        print(num1+num2)
-    elif(opp==2):
-        print(num1-num2)
-    elif(opp==3):
-        print(num1*num2)
-    elif(opp==4):
-        print(num1/num2)
-    elif(opp==5):
-        print(num1**num2)
-    elif(opp==6):
-        break
-    else:
-        print("Invalid");
+#     if(opp==1):
+#         print(num1+num2)
+#     elif(opp==2):
+#         print(num1-num2)
+#     elif(opp==3):
+#         print(num1*num2)
+#     elif(opp==4):
+#         print(num1/num2)
+#     elif(opp==5):
+#         print(num1**num2)
+#     elif(opp==6):
+#         break
+#     else:
+#         print("Invalid");
+
+apple = "harry";
+print(apple[-4:-2])
